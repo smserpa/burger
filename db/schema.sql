e8b50d6cad189466a68af9879bdc32433957e613
Stefanie Serpa <smserpa@gmail.com>
2020-10-15 09:48:13 -0700

    initial home page view and db setup

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Drops the burgers_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Creates the database burgers_db and specifies it for use.
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Creates the table burgers.
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(255),
+  devoured bool,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('BLTA Burger');
