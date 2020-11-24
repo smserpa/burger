fe4c80ba6f44fe97292ad27e28c3b94606e13fc4
Stefanie Serpa <smserpa@gmail.com>
2020-11-23 20:46:00 -0800

    moved get route into burgersController.js and added route to static html

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const connection = require("./config/connection");
+const routes = require("./controllers/burgersController");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,15 +14,7 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.get("/", (req,res) => {
-    connection.query("SELECT * FROM burgers", (err, data) => {
-        if (err) {
-            return err;
-        }
-        console.log(data);
-        res.render("index", { burgers: data });
-    })
-})
+app.use(routes);
 
 app.listen(PORT, () => {
 
