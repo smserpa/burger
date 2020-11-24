bc106b19cd45edb56b9935908b69b3839a22cf1b
Stefanie Serpa <smserpa@gmail.com>
2020-11-23 20:44:14 -0800

    finialized orm structure

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,56 @@
 const connection = require("./connection.js");
+
+const orm = {
+
+    selectAll: (table, cb) => {
+        
+        const queryString = "SELECT * FROM ??";
+        connection.query(queryString, [table], (err, data) => {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
+
+    insertOne: (table, newRowData, cb) => {
+
+        const queryString = "INSERT INTO ?? SET ?";
+        const values = [table, newRowData];
+
+        connection.query(queryString, values, (err, data) => {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
+
+    updateOne: (table, updateValues, condition, cb) => {
+
+        const queryString = "UPDATE ?? SET ? WHERE ? LIMIT 1";
+        const values = [table, updateValues, condition];
+
+        connection.query(queryString, values, (err, data) => {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
+
+    deleteOne: (table, condition, cb) => {
+
+        const queryString = "DELETE FROM ?? WHERE ? LIMIT 1";
+        const values = [table, condition];
+
+        connection.query(queryString, values, (err, data) => {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
+};
+
+module.exports = orm;
